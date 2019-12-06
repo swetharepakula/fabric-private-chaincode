3234e7f7ba35eb4534071fa5941ff110eb8bbf7a
Bruno Vavala <bruno.vavala@intel.com>
2019-12-06 07:08:38 -0800

    rename folder to chaincode

---
+++ demo/chaincode/bidder.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2019 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+namespace ClockAuction
+{
+    class Bidder
+    {
+        uint32_t requiredEligibilityPercentage;
+    };
+}
